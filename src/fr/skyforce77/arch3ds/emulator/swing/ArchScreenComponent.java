f3589dc79c8571d6aa9706e0410b74157a5af10c
Skyforce77 <arnaud.bruyer@gmail.com>
2015-03-21 22:27:51 +0100

    Implemented touchscreen

--- src/fr/skyforce77/arch3ds/emulator/swing/ArchScreenComponent.java
+++ src/fr/skyforce77/arch3ds/emulator/swing/ArchScreenComponent.java
@@ -2,6 +2,8 @@ package fr.skyforce77.arch3ds.emulator.swing;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JComponent;
 
@@ -9,7 +11,7 @@ import fr.skyforce77.arch3ds.api.graphics.ArchGraphics;
 import fr.skyforce77.arch3ds.api.graphics.ArchScreen;
 import fr.skyforce77.arch3ds.emulator.ArchGameManager;
 
-public class ArchScreenComponent extends JComponent{
+public class ArchScreenComponent extends JComponent implements MouseListener{
 	
 	private static final long serialVersionUID = -8493615357999345644L;
 	
@@ -26,5 +28,23 @@ public class ArchScreenComponent extends JComponent{
 			ArchGameManager.getArchGame().drawScreen(new ArchGraphics(screen, g2d, getWidth(), getHeight()));
 		}
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(ArchGameManager.getArchGame() != null)
+			ArchGameManager.getArchGame().onTouched(screen, e.getX(), e.getY());
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+
+	@Override
+	public void mouseExited(MouseEvent e) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {}
 
 }
