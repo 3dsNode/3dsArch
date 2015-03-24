6039bbbcf20eae9c8ab2004992e1f1f37b8fd0a8
Skyforce787 <arnaud.bruyer@gmail.com>
2015-03-24 11:48:17 +0100

    Added displayPopup method

--- src/fr/skyforce77/arch3ds/api/GameManager.java
+++ src/fr/skyforce77/arch3ds/api/GameManager.java
@@ -1,5 +1,9 @@
 package fr.skyforce77.arch3ds.api;
 
+import javax.swing.JOptionPane;
+
+import fr.skyforce77.arch3ds.emulator.Emulator;
+
 public class GameManager {
 
 	/**
@@ -8,6 +12,15 @@ public class GameManager {
      * @return boolean method exists and called successfully
      */
 	public static boolean callGameMethod(String method, Object... args){
+		//Display computer popup
+		if(method.equals("displayPopup")) {
+			if(args.length >= 1) {
+				JOptionPane.showMessageDialog(Emulator.getBottomScreen(), args[0],
+					args.length >= 2 ? args[1].toString() : "Information",
+					JOptionPane.INFORMATION_MESSAGE);
+				return true;
+			}
+		}
 		return false;
 	}
 	
